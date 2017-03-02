9ebaf7be038ca9d652501c235a9384d13edfb44f
YongcunPan <11007806@qq.com>
2017-03-02 16:14:34 +0800

    完成首页和列表页
    完成首页和列表页

---
+++ js/init.js
@@ -0,0 +1,28 @@
+$(function(){
+  initauto();
+});
+
+function initauto() {
+
+  $(".menu li,.labels label").each(function(){
+    $(this).click(function(){
+      $(this).addClass("active").siblings().removeClass("active");
+    });
+  });
+
+  $(".slideBox").slide({autoPlay:true});
+
+  $(".slideBoxList").slide({
+    startFun:function(i,c){
+        $(".slideBoxList .bottomImg li").eq(i).addClass("active").siblings().removeClass("active");
+    }
+  });
+  
+  $(".slideParBox .slideChiBox").slide({ mainCell:"ul",vis:4,prevCell:".sPrev",nextCell:".sNext",effect:"leftLoop"});
+  jQuery(".slideParBox").slide({titCell:".parHd li",mainCell:".parBd"});
+
+  $(".iListModule li a").mouseenter(function(){
+    $(this).parent().addClass("on").siblings().removeClass("on");;
+  });
+
+}
